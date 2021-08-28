b54078b5d272057218e4e40d8aefc555a6d0fcfc
VioletViola <vivian.v18723@gmail.com>
2021-08-28 18:03:03 +0100

    Create constructor, methods and instance variables for game.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
+        NoughtsAndCrosses noughtsAndCrosses = new NoughtsAndCrosses();
     }
 }
--- src/NoughtsAndCrosses.java
+++ src/NoughtsAndCrosses.java
@@ -1,2 +1,39 @@
-public class NoughtsAndCrosses {
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+public class NoughtsAndCrosses implements ActionListener {
+
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1_turn;
+
+    NoughtsAndCrosses(){
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void firstTurn(){
+
+    }
+
+    public void check(){
+
+    }
+
+    public void xWins(int a, int b, int c){
+
+    }
+
+    public void oWins(int a, int b, int c){
+
+    }
+
 }
