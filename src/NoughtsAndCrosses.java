18841dc5a721fc800238b9e5f84a2577ce3725b9
VioletViola <vivian.v18723@gmail.com>
2021-08-28 23:03:41 +0100

    Display wining combination in green & ensure game ends.

--- src/NoughtsAndCrosses.java
+++ src/NoughtsAndCrosses.java
@@ -145,9 +145,24 @@ public class NoughtsAndCrosses implements ActionListener {
     }
 
     public void xWins(int a, int b, int c){
+        buttons[a].setBackground(Color.GREEN);
+        buttons[b].setBackground(Color.GREEN);
+        buttons[c].setBackground(Color.GREEN);
 
+        for (int i = 0; i < 9; i++) {
+            buttons[i].setEnabled(false);
+        }
+        textfield.setText("X wins");
     }
 
     public void oWins(int a, int b, int c){
+        buttons[a].setBackground(Color.GREEN);
+        buttons[b].setBackground(Color.GREEN);
+        buttons[c].setBackground(Color.GREEN);
+
+        for (int i = 0; i < 9; i++) {
+            buttons[i].setEnabled(false);
+        }
+        textfield.setText("O wins");
     }
 }
