61848dfa8cae981da7bac358bed8e3b3d836aa0e
VioletViola <vivian.v18723@gmail.com>
2021-08-28 22:58:02 +0100

    Add all possible combinations of x and o on the board.

--- src/NoughtsAndCrosses.java
+++ src/NoughtsAndCrosses.java
@@ -91,9 +91,61 @@ public class NoughtsAndCrosses implements ActionListener {
     }
 
     public void check(){
+        // check if X wins
+        if ((buttons[0].getText() == "X") && (buttons[1].getText() == "X") && (buttons[0].getText() == "X")){
+            xWins(0, 1, 2);
+        }
+        if ((buttons[3].getText() == "X") && (buttons[4].getText() == "X") && (buttons[5].getText() == "X")){
+            xWins(3, 4, 5);
+        }
+        if ((buttons[6].getText() == "X") && (buttons[7].getText() == "X") && (buttons[8].getText() == "X")){
+            xWins(6, 7, 8);
+        }
+        if ((buttons[0].getText() == "X") && (buttons[3].getText() == "X") && (buttons[6].getText() == "X")){
+            xWins(0, 3, 6);
+        }
+        if ((buttons[1].getText() == "X") && (buttons[4].getText() == "X") && (buttons[7].getText() == "X")){
+            xWins(1, 4, 7);
+        }
+        if ((buttons[2].getText() == "X") && (buttons[5].getText() == "X") && (buttons[8].getText() == "X")){
+            xWins(2, 5, 8);
+        }
+        if ((buttons[0].getText() == "X") && (buttons[4].getText() == "X") && (buttons[8].getText() == "X")){
+            xWins(0, 4, 8);
+        }
+        if ((buttons[2].getText() == "X") && (buttons[4].getText() == "X") && (buttons[6].getText() == "X")){
+            xWins(2, 4, 6);
+        }
+
+        // check if O wins
+        if ((buttons[0].getText() == "O") && (buttons[1].getText() == "O") && (buttons[0].getText() == "O")){
+            oWins(0, 1, 2);
+        }
+        if ((buttons[3].getText() == "O") && (buttons[4].getText() == "O") && (buttons[5].getText() == "O")){
+            oWins(3, 4, 5);
+        }
+        if ((buttons[6].getText() == "O") && (buttons[7].getText() == "O") && (buttons[8].getText() == "O")){
+            oWins(6, 7, 8);
+        }
+        if ((buttons[0].getText() == "O") && (buttons[3].getText() == "O") && (buttons[6].getText() == "O")){
+            oWins(0, 3, 6);
+        }
+        if ((buttons[1].getText() == "O") && (buttons[4].getText() == "O") && (buttons[7].getText() == "O")){
+            oWins(1, 4, 7);
+        }
+        if ((buttons[2].getText() == "O") && (buttons[5].getText() == "O") && (buttons[8].getText() == "O")){
+            oWins(2, 5, 8);
+        }
+        if ((buttons[0].getText() == "O") && (buttons[4].getText() == "O") && (buttons[8].getText() == "O")){
+            oWins(0, 4, 8);
+        }
+        if ((buttons[2].getText() == "O") && (buttons[4].getText() == "O") && (buttons[6].getText() == "O")){
+            oWins(2, 4, 6);
+        }
     }
 
     public void xWins(int a, int b, int c){
+
     }
 
     public void oWins(int a, int b, int c){
