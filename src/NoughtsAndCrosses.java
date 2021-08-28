fb5c6b50ae0d20330ebce5d38181fd2592b80051
VioletViola <vivian.v18723@gmail.com>
2021-08-28 22:24:20 +0100

    Add if statements to allow X's and O's to appear on the grid.

--- src/NoughtsAndCrosses.java
+++ src/NoughtsAndCrosses.java
@@ -49,7 +49,27 @@ public class NoughtsAndCrosses implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        for (int i = 0; i < 9; i++) {
+            if(e.getSource() == buttons[i]){
+                if (player1_turn) {
+                    if (buttons[i].getText() == "") {
+                        buttons[i].setForeground(new Color(255, 0, 0));
+                        buttons[i].setText("X");
+                        player1_turn = false;
+                        textfield.setText("O turn");
+                        check();
+                    }
+                } else {
+                    if (buttons[i].getText() == "") {
+                        buttons[i].setForeground(new Color(0, 0, 255));
+                        buttons[i].setText("O");
+                        player1_turn = true;
+                        textfield.setText("X turn");
+                        check();
+                    }
+                }
+            }
+        }
     }
 
     public void firstTurn(){
@@ -71,15 +91,11 @@ public class NoughtsAndCrosses implements ActionListener {
     }
 
     public void check(){
-
     }
 
     public void xWins(int a, int b, int c){
-
     }
 
     public void oWins(int a, int b, int c){
-
     }
-
 }
